7ef45097da02e73065c19e6dffe4001b4b738b1f
Karanvir <iamkaranvir93@gmail.com>
2019-01-04 01:48:53 -0500

    Added dependency common.lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,13 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+	
+	public int countWords(String words) {
+		String[] allWords = StringUtils.split(words, ' ');
+		return (allWords == null) ? 0 : allWords.length;
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +17,7 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	int count = app.countWords("I live in Canada");
+	System.out.println("Total number of words: "+count);
     }
 }
